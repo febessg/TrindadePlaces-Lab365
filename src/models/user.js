7dfbee143e72be9c7591cc6c193263f3062ab05e
Fernanda Bess Gonçalves <fernanda-bess@hotmail.com>
2023-04-11 16:07:20 -0300

    add model

---
+++ src/models/user.js
@@ -0,0 +1,35 @@
+//[M1S10] Ex 01 - Criando modelo User
+
+const Sequelize = require('sequelize');
+
+const connection = require('../database/index');
+
+const User = connection.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [8,42]
+        }
+    }
+});
